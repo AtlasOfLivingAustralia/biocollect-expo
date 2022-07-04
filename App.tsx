26840d173ead71f04b9b31a70fafc4a29360af5c
Jack Brinkman <jack.brinkman@csiro.au>
2022-07-04 12:44:06 +1000

    Removed unused Lato fonts

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text } from 'react-native';
+import { useFonts } from 'expo-font';
 
 // Import screen components
 import { Home } from './src/screens';
@@ -7,6 +9,14 @@ import { Home } from './src/screens';
 const Stack = createNativeStackNavigator();
 
 function App() {
+  // Load the Lato font
+  const [fontsLoaded, fontsError] = useFonts({
+    Lato: require('./assets/fonts/Lato.ttf')
+  });
+
+  if (!fontsLoaded) return <Text>Loading fonts...</Text>;
+  if (fontsError) return <Text>Font Error!</Text>;
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
